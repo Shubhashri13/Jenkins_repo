ddde350811ddb83f2b727670d1fffd4e2171c1fd
Pawan <Pawan@LAPTOP-L8DJVMDE>
2025-02-06 14:00:35 +0530

    PollScm

---
+++ SimpleJenkins/src/test/java/demo1/PollScm.java
@@ -0,0 +1,14 @@
+package demo1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollScm
+{
+	@Test
+	public void scmPoll()
+	{
+		Reporter.log("PollScm executed",true);
+	}
+
+}
